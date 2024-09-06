acd1df04d16335396cda3ceabc88d5aa1cedc2e1
rahulkate173 <rahulkate173@gmail.com>
2024-09-06 21:43:03 +0530

    map,filter,reduce (deep drive)

---
+++ README.md
@@ -0,0 +1,3 @@
+Author : RAHUL RAVINDRA KATE
+
+Description : It contain the full use of map,filter,reduce function with example 
---
+++ map,filter,reduce deep drive .py
@@ -0,0 +1,18 @@
+from functools import reduce # imported it to use reduce function 
+lst = [1,2,3,4,5,6,7,8,9]
+# map in python
+# results = map(lambda x:x*2 ,lst)
+# # print(result)     <--- return map object 
+# for result in results :
+#     print(result)
+
+# reduce in python 
+#form unctools import reduce 
+# results = reduce(lambda x,y: x+y , lst)
+# # print(results)
+
+# # filter in python 
+# results = filter(lambda x: x%2==0 , lst)
+# # print(results)
+# for result in results:
+#     print(result)
